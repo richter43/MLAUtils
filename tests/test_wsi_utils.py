635ea836f39b9ab4398fc7f4a94eb05a1e152b87
Samuel Oreste Abreu <orestsam@protonmail.com>
2022-06-06 00:34:54 +0200

    Feat: SlideManager assigns label given an ASAP XML

--- tests/test_wsi_utils.py
+++ tests/test_wsi_utils.py
@@ -2,11 +2,12 @@ import sys
 import os
 import json
 import unittest
-from src import DatasetManager
+from src import DatasetManager, SlideMetadata
 
 module_dir = os.path.dirname(__file__)
 with open(os.path.join(module_dir, "paths.json")) as f:
     paths_json = json.load(f)
+    XML_FILENAME = paths_json['XML_FILENAME']
     ccRCC_FILEPATH = paths_json["SLIDE_FILENAME"]
 
 class TestWSIUtils(unittest.TestCase):
@@ -14,8 +15,10 @@ class TestWSIUtils(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.filepaths = [ccRCC_FILEPATH]
-        cls.labels = ["ccRCC", "not-cancer"]
-        cls.tile_size = 128
+        cls.xml_paths = [XML_FILENAME]
+        cls.tile_size = 1024
 
     def test_dataset_manager(self):
-        dm = DatasetManager(self.filepaths, self.labels, self.tile_size)
+
+        slide_metadata_list = [SlideMetadata(wsi_path, xml_path=xml_path) for wsi_path, xml_path in zip(self.filepaths, self.xml_paths)]
+        dm = DatasetManager(slide_metadata_list, self.tile_size)
