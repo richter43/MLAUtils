41e3855a81d020fed47edce91509b669daae11d4
Samuel Oreste Abreu <orestsam@protonmail.com>
2022-06-06 15:16:39 +0200

    Feat: DatasetManager is now in full working order

--- tests/test_wsi_utils.py
+++ tests/test_wsi_utils.py
@@ -2,7 +2,8 @@ import sys
 import os
 import json
 import unittest
-from src import DatasetManager, SlideMetadata
+from src import DatasetManager, SlideMetadata, RenalCancerType, get_label_from_path
+import matplotlib.pyplot as plt
 
 module_dir = os.path.dirname(__file__)
 with open(os.path.join(module_dir, "paths.json")) as f:
@@ -16,9 +17,16 @@ class TestWSIUtils(unittest.TestCase):
     def setUpClass(cls):
         cls.filepaths = [ccRCC_FILEPATH]
         cls.xml_paths = [XML_FILENAME]
-        cls.tile_size = 1024
+        cls.tile_size = 128
+        cls.slide_metadata_list = [SlideMetadata(wsi_path, xml_path=xml_path, label=get_label_from_path(wsi_path)) for wsi_path, xml_path in zip(cls.filepaths, cls.xml_paths)]
 
     def test_dataset_manager(self):
+        dm = DatasetManager(self.slide_metadata_list, self.tile_size)
 
-        slide_metadata_list = [SlideMetadata(wsi_path, xml_path=xml_path) for wsi_path, xml_path in zip(self.filepaths, self.xml_paths)]
-        dm = DatasetManager(slide_metadata_list, self.tile_size)
+    def test_create_dataset(self):
+        dm = DatasetManager(self.slide_metadata_list, self.tile_size)
+        dataset = dm.make_dataset()
+        for image_batch, label_batch in dataset:
+            plt.imshow(image_batch[0])
+            plt.show()
+            break
